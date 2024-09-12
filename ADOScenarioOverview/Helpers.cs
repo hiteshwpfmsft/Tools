e862e8a54037e6dcb953be88bdf32bf03bb680d2
Hitesh Kumar <hik@microsoft.com>
2024-09-12 13:06:15 +0530

    Introducing ADO Scenario Overview tool

---
+++ ADOScenarioOverview/Helpers.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+
+namespace ScenarioOverview
+{
+    public static class Helpers
+    {
+        public static int GetRemainingWorkingDays(DateTime endDate, DateTime currentDate)
+        {
+            if (currentDate > endDate)
+                return 0;
+
+            int remainingDays = 0;
+
+            for (var date = currentDate; date <= endDate; date = date.AddDays(1))
+            {
+                if (date.DayOfWeek != DayOfWeek.Saturday && date.DayOfWeek != DayOfWeek.Sunday)
+                {
+                        remainingDays++;
+                }
+            }
+
+            return remainingDays;
+        }
+
+        public static double GetDoubleField(JObject workItemDetails, string fieldName)
+        {
+            return workItemDetails["fields"]?[fieldName]?.Value<double>() ?? 0;
+        }
+
+        public static string? GetStringField(JObject workItemDetails, string fieldName)
+        {
+            if(fieldName == "System.AssignedTo")
+            {
+                return workItemDetails["fields"]?["System.AssignedTo"]?["displayName"]?.Value<string>();
+            }
+
+            return workItemDetails["fields"]?[fieldName]?.Value<string>();
+        }
+
+    }
+}
---
+++ ADOScenarioOverview/Models.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+
+namespace ScenarioOverview
+{
+    public class ContributorData
+    {
+        public double OriginalEstimate { get; set; } = 0;
+        public double RemainingDays { get; set; } = 0;
+        public List<TaskData> TaskDetails { get; set; } = new List<TaskData>();
+    }
+
+    public class TaskData
+    {
+        public string TaskId { get; set; } = string.Empty;
+        public string TaskTitle { get; set; } = string.Empty;
+        public double OriginalEstimate { get; set; } = 0;
+        public double RemainingDays { get; set; } = 0;
+        public string Owner { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+        public string Iteration { get; set; } = string.Empty;
+    }
+
+    public class DeliverableData
+    {
+        public string DeliverableId { get; set; } = string.Empty;
+        public string DeliverableTitle { get; set; } = string.Empty;
+        public double OriginalEstimate { get; set; } = 0;
+        public double RemainingDays { get; set; } = 0;
+        public string Owner { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+        public string Iteration { get; set; } = string.Empty;
+    }
+
+
+    public class ContributorSummary
+    {
+        public string Contributor { get; set; } = string.Empty;
+        public double OriginalEstimate { get; set; } = 0;
+        public double CompletedDays { get; set; } = 0;
+        public double RemainingDays { get; set; } = 0;
+        public double TotalCapacity { get; set; } = 0;
+    }
+
+    public class CapacitySummary
+    {
+        public string Contributor { get; set; } = string.Empty;
+        public double Totalworkingdays { get; set; } = 0;
+        public double Multiplier { get; set; } = 0;
+        public double TotalCapacity { get; set; } = 0;
+    }
+}
